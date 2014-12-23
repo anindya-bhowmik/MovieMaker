ca475d8150ca3491849deb8509bf65c709d427cf
anindya <anindya.csesust@gmail.com>
2014-12-24 00:05:10 +0600

    Initial commit

---
+++ Moviemaker/Moviemaker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Moviemaker
+//
+//  Created by Anindya on 12/24/14.
+//  Copyright (c) 2014 Anindya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Moviemaker/Moviemaker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Moviemaker
+//
+//  Created by Anindya on 12/24/14.
+//  Copyright (c) 2014 Anindya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
